9e2e2c8fab7d52d0778aadad88a0adacff8bfe90
Mark <mark.duplock@gmail.com>
2021-03-09 19:23:15 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 def prime_checker(number) :
     counter = 0
-    for i in range(1, 101):
+    for i in range(1, number + 1):
         if number % i == 0:
             counter += 1
     if counter > 2:
